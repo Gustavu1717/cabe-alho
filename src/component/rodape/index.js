44c57c475a64a2b250c7097a1620e15d90a132c2
jorgestark <jorgestark278@gmail.com>
2023-09-14 16:39:21 -0300

    publiquei

--- src/component/rodape/index.js
+++ src/component/rodape/index.js
@@ -1,10 +1,51 @@
 import './index.scss'
+import { Link  } from 'react-router-dom'
 
 
 export default function Rodape(){
     return(
-        <div className='comp-'>
 
-        </div>
+       <div className='comp-rodape'>
+             <aside className='conteudo'>
+            <aside className='informacoes'>
+              <h2>AJUDA</h2>  
+               <Link>Atendimento ao Cliente</Link> 
+               <Link>Dúvidas Frequentes</Link> 
+               <Link>Políticas de Privacidade</Link> 
+
+            </aside>
+
+            <aside className='informacoes'>
+                <h2>INFORMAÇÕES CORPORATIVAS</h2>
+                <Link>O que nós somos</Link>
+                <Link>Contato</Link>
+            </aside>           
+
+            <aside className='informacoes'>
+                <h2>FORMAS DE PAGAMENTO</h2>
+                <div>
+                    <img src='./assets/images/cartao.png'></img>
+                    <img src='./assets/images/elo.png'></img>
+                    <img src='./assets/images/visa.png'></img>
+
+                </div>
+            </aside>            
+
+            <aside className='informacoes'>
+                <h2>REDES SOCIAIS</h2>
+                <div className='rede'>
+                    <img src='./assets/images/instagram.png'></img>
+                    <img src='./assets/images/facebook.png'></img>
+                    <img src='./assets/images/whatsapp.png'></img>
+
+                </div>
+            </aside>           
+            
+           
+        </aside>
+        <hr></hr>
+
+        <p>Seus dados pessoais serão respeitados de acordo com a nossa política de privacidade. *Nada será publicado em sua timeline. Serviço válido somente para pessoas físicas.Em caso de dúvidas, acesse nossa central de atendimento.</p>
+       </div>
     )
-}
+}          
--- src/pages/tela 1/index.js
+++ src/pages/tela 1/index.js
@@ -1,15 +1,15 @@
 import './index.scss'
 
-import Cabecalho from '../../component'
+import Cabecalho from '../../component/cabecalho'
+import Rodape from '../../component/rodape'
 
 export default function tela (){
     return(
         <div className='tela1'>
             <Cabecalho/>
-
-            <aside>
-                
-            </aside>
+        
+        
+            <Rodape />
         </div>
     )
 }
