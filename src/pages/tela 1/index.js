4fac4a8715779f2651668d75632566f68b0e154d
Gustavo <gstvsteixeira@gmail.com>
2023-09-15 15:56:25 -0300

    atualizacoes

--- src/pages/tela 1/index.js
+++ src/pages/tela 1/index.js
@@ -1,13 +1,36 @@
-import './index.scss'
+import './index.scss';
 
-import Cabecalho from '../../component/cabecalho'
-import Rodape from '../../component/rodape'
+import Cabecalho from '../../component/cabecalho';
+import Rodape from '../../component/rodape';
+
 
 export default function tela (){
+
+
+
+
+
+
+
+
     return(
-        <div className='tela1'>
+        <div className='pagina-carrinho'>
             <Cabecalho/>
-        
+            <div className='container'>
+                <div className='bt-carrinho'>
+                    <p> Meu Carrinho </p>
+                </div>
+
+            <div className='carrinho'>
+            <div className='ps'>
+                <img src='/assets/images/carrinho2.png'/>
+                    <p id='negrito'> Seu carrinho esta vazio... </p>
+                    <p id='nor'> Que tal navegar na loja e encontrar algum <br/> produto que você gosta? </p>
+                    </div>
+                
+            </div>
+            <button> Adicione Item </button>
+            </div>
         
             <Rodape />
         </div>
